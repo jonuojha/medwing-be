f0895bc1d1fa7a8221523ea10d747c8773f2bc54
prabhajo <prabhajo@PRINHYLTPAP1070.local>
2019-03-31 23:41:35 +0530

    added geocode

--- grails-app/controllers/medwing/be/GeocodeController.groovy
+++ grails-app/controllers/medwing/be/GeocodeController.groovy
@@ -2,12 +2,20 @@ package medwing.be
 
 import grails.converters.JSON
 import medwing.be.provider.IGeoProvider
+import org.springframework.http.HttpStatus
 
 class GeocodeController {
 
     IGeoProvider geocodeProvider
 
     def index() {
-        render(geocodeProvider.getGeocode(params.address as String) as JSON)
+        try {
+            List<Marker> markers = geocodeProvider.getGeocode(params.address as String)
+            render(markers as JSON)
+        } catch (Exception e) {
+            response.status = HttpStatus.INTERNAL_SERVER_ERROR.value()
+            render([message: 'Failed to get geocodes', error: e.message] as JSON)
+        }
+
     }
 }
